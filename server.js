64ba53eed802891e4632f48c57aa1779b9f90372
taeyoung kim <taeyokim@gmail.com>
2015-11-23 16:09:02 +0900

    added image and edit code

--- server.js
+++ server.js
@@ -4,3 +4,17 @@ http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Welcome to 1 World\n');
 }).listen(port);
+
+
+var http = require('http')
+  , fs = require('fs');
+var port = process.env.PORT || 1337;
+
+fs.readFile('image.jpg', function(err, data) {
+  if (err) throw err; // Fail if the file can't be read.
+  http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'image/jpeg'});
+    res.end(data); // Send the file data to the browser.
+  }).listen(port);
+  //console.log('Server running at http://localhost:8124/');
+});
