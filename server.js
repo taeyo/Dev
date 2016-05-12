c636748f6103a7437c7660c2dd2d76e031bbbb71
Taeyo <taeyokim@gmail.com>
2016-05-12 17:52:53 +0900

    이미지로 변경. 진짜임. 지금하는거여

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-// Demo Code
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Welcome to Auzre Discovery Day\n');
-}).listen(port);   
-
-
+// // Demo Code
 // var http = require('http')
-//   , fs = require('fs'); 
 // var port = process.env.PORT || 1337;
+// http.createServer(function(req, res) {
+//   res.writeHead(200, { 'Content-Type': 'text/plain' });
+//   res.end('Welcome to Auzre Discovery Day\n');
+// }).listen(port);   
 
-// fs.readFile('azure.jpg', function(err, data) {
-//   if (err) throw err; // Fail if the file can't be read.
-//   http.createServer(function(req, res) {
-//     res.writeHead(200, {'Content-Type': 'image/jpeg'});
-//     res.end(data); // Send the file data to the browser.
-//   }).listen(port);
-//   //console.log('Server running at http://localhost:8124/');
-// });
+
+var http = require('http')
+  , fs = require('fs'); 
+var port = process.env.PORT || 1337;
+
+fs.readFile('azure.jpg', function(err, data) {
+  if (err) throw err; // Fail if the file can't be read.
+  http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'image/jpeg'});
+    res.end(data); // Send the file data to the browser.
+  }).listen(port);
+  //console.log('Server running at http://localhost:8124/');
+});
 
 //git push azure master
